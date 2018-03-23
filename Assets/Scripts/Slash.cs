d8950ef4b6a5b3f3f6a7fccbf2d1c777bb7f374f
Chris Commisso <qwertykeebored@live.com>
2018-03-23 00:31:43 -0400

    changes
    ch-ch-changes

---
+++ Assets/Scripts/Slash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slash : MonoBehaviour {
+    float Vel = 0f;
+    Vector3 prevPos = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public float Damage = 20;
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vel = (prevPos - transform.position).magnitude;
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Health>() != null&&Vel>4)
+        {
+            collision.gameObject.GetComponent<Health>().Damage(Damage);
+        }
+    }
+}
