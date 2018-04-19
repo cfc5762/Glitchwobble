32953732d305706e3ba6446fcaf5b0a9d44de15c
Chris Commisso <qwertykeebored@live.com>
2018-04-19 17:53:47 -0400

    kk
    kk

--- Assets/Scripts/GroundScript.cs
+++ Assets/Scripts/GroundScript.cs
@@ -11,6 +11,7 @@ public class GroundScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        transform.position = new Vector3(Head.head.transform.position.x, transform.position.y, Head.head.transform.position.z);
         Debug.DrawLine(transform.position,transform.position - (transform.up * 2f), Color.blue);
         if (Physics.Raycast(transform.position, -transform.up, 2f))
         {
